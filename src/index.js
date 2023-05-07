d099d0d4ab38d128c53efcd4ef711a60bd5e1aaa
sanandress22000 <128901607+sanandress22000@users.noreply.github.com>
2023-05-08 08:11:11 +0900

    0508

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css";
+
+//const 関数宣言
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
